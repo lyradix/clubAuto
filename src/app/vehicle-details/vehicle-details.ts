89b09deceb3aa8ce8665eeb29c77ee08d544b2af
lyradix <daryl.stpaul@gmail.com>
2025-06-30 11:33:22 +0200

    vehicle details branch added

---
+++ src/app/vehicle-details/vehicle-details.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-vehicle-details',
+  imports: [],
+  templateUrl: './vehicle-details.html',
+  styleUrl: './vehicle-details.css'
+})
+export class VehicleDetails {
+
+}
