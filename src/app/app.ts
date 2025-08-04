06adf6b1c5d3cc14c9b1d655c99f4e9eb891e34e
lyradix <daryl.stpaul@gmail.com>
2025-08-04 20:14:31 +0200

    footer added

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Home } from './home/home';
+import { Footer } from "./footer/footer";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Home],
+  imports: [RouterOutlet, Home, Footer],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
