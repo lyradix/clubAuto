55b6b4bc56fb52ebc5339a177322ad98683505d4
lyradix <daryl.stpaul@gmail.com>
2025-08-05 15:39:39 +0200

    navbar added

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Home } from './home/home';
 import { Footer } from "./footer/footer";
+import { NavBar } from './nav-bar/nav-bar';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Home, Footer],
+  imports: [RouterOutlet, NavBar,Home, Footer],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
