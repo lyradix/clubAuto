7c3157d1fd2e9b9a50af3b40cd5d95d26313e76f
lyradix <daryl.stpaul@gmail.com>
2025-08-13 18:12:13 +0200

    deploy new version with menu burger on mobile version

--- dist/clubauto/server/angular-app-manifest.mjs
+++ dist/clubauto/server/angular-app-manifest.mjs
@@ -25,11 +25,11 @@ export default {
 ],
   entryPointToBrowserMapping: undefined,
   assets: {
-    'index.csr.html': {size: 9479, hash: '096cc5d28ffa1e70311e8eab271049649e7070c6e543964a0e5b4d0c98f278c8', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 1005, hash: '273ef32f90b441015c3571b665269bd9e9565b30459124c265f12f1f00cc4f96', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
-    'vehicle-details/index.html': {size: 73791, hash: 'aea61d0f1ab3a039653d40090b1c5a49e6af83eae3c1992cbfc9302f28269fc4', text: () => import('./assets-chunks/vehicle-details_index_html.mjs').then(m => m.default)},
-    'contact/index.html': {size: 97909, hash: '011f58f7a92c2dab12a535109e88121d6f4a4ded31c948ddb4d75fda3229a016', text: () => import('./assets-chunks/contact_index_html.mjs').then(m => m.default)},
-    'index.html': {size: 110379, hash: '3b71fda199681b3a570e5a316705712b2a801945bf26f4e964d1a516841648a1', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
-    'styles-XISE5V6I.css': {size: 396879, hash: 'gLVeo8Jxijw', text: () => import('./assets-chunks/styles-XISE5V6I_css.mjs').then(m => m.default)}
+    'index.csr.html': {size: 19529, hash: '1eebddcb53581593d972afa19f71143399faa172ebf4e6486a42b8dd0b2704cd', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 1005, hash: 'b37d03b02ab07146682288d99a3509b5ba6dd858533674822331db376d62a4b4', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'vehicle-details/index.html': {size: 102372, hash: '916dd383572490fba4dacea193fdaa9622cc74d1f1ff212f044a2ed4c7c36ae6', text: () => import('./assets-chunks/vehicle-details_index_html.mjs').then(m => m.default)},
+    'contact/index.html': {size: 127182, hash: '127de1d213bfd8d685bb44250139ac94380342ffdda696592a0e6b2af213476f', text: () => import('./assets-chunks/contact_index_html.mjs').then(m => m.default)},
+    'index.html': {size: 118067, hash: 'e6cdd87693ac6709b9f3587b31f1a5c47574a83d449998a16f350ecf8e9b481c', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
+    'styles-P7C6FOJ7.css': {size: 422484, hash: 'yB5i+3cQFRI', text: () => import('./assets-chunks/styles-P7C6FOJ7_css.mjs').then(m => m.default)}
   },
 };
