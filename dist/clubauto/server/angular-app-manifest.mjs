77dd70415cfd7125fef3a85e1da741c98be36b80
lyradix <daryl.stpaul@gmail.com>
2025-09-08 11:41:11 +0200

    fix police on small device for home

--- dist/clubauto/server/angular-app-manifest.mjs
+++ dist/clubauto/server/angular-app-manifest.mjs
@@ -25,11 +25,11 @@ export default {
 ],
   entryPointToBrowserMapping: undefined,
   assets: {
-    'index.csr.html': {size: 19529, hash: '1eebddcb53581593d972afa19f71143399faa172ebf4e6486a42b8dd0b2704cd', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 1005, hash: 'b37d03b02ab07146682288d99a3509b5ba6dd858533674822331db376d62a4b4', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
-    'vehicle-details/index.html': {size: 102372, hash: '916dd383572490fba4dacea193fdaa9622cc74d1f1ff212f044a2ed4c7c36ae6', text: () => import('./assets-chunks/vehicle-details_index_html.mjs').then(m => m.default)},
-    'contact/index.html': {size: 127182, hash: '127de1d213bfd8d685bb44250139ac94380342ffdda696592a0e6b2af213476f', text: () => import('./assets-chunks/contact_index_html.mjs').then(m => m.default)},
-    'index.html': {size: 118067, hash: 'e6cdd87693ac6709b9f3587b31f1a5c47574a83d449998a16f350ecf8e9b481c', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
-    'styles-P7C6FOJ7.css': {size: 422484, hash: 'yB5i+3cQFRI', text: () => import('./assets-chunks/styles-P7C6FOJ7_css.mjs').then(m => m.default)}
+    'index.csr.html': {size: 19529, hash: '5ca47cdf8a0f9de58e4c629744c31f6f17f75985a243a9429d2091429425cbfe', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 1005, hash: '3199bccdb6d4eadfe7d2257a7d8960f53ba53124b80c598e8b2f06b10b85bf8f', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'contact/index.html': {size: 127465, hash: '278df3e9705414d90cdfecd8266d321b4453aefee2c4c9343f6b08430a54d7f5', text: () => import('./assets-chunks/contact_index_html.mjs').then(m => m.default)},
+    'vehicle-details/index.html': {size: 102655, hash: '4c5e42abdb4c88594b07bccaa8cfde97c84687c0f97a3379c73c080ffda9982d', text: () => import('./assets-chunks/vehicle-details_index_html.mjs').then(m => m.default)},
+    'index.html': {size: 118562, hash: 'cc765cc11ce815d0aa533ac364a0f58a8f251f34b10dec6fd9623238ee76ddbf', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
+    'styles-BEXXJVGV.css': {size: 422484, hash: '4lJV10rc7bs', text: () => import('./assets-chunks/styles-BEXXJVGV_css.mjs').then(m => m.default)}
   },
 };
